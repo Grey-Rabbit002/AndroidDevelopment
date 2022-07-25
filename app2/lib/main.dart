30b9314cccfef82e498d8d984417ced0335fe71d
Arjun <arjun.saxena.as2002@gmail.com>
2022-07-25 15:06:39 +0530

    Expenses App Working Started

--- app2/lib/main.dart
+++ app2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +14,29 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      amount: 1423.23,
+      title: "Shoes",
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      amount: 723.23,
+      title: "Groceries",
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter App"),
+        title: const Text("Personal Expenses App"),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Card(
             elevation: 10,
@@ -29,26 +44,54 @@ class MyHomePage extends StatelessWidget {
             child: Container(
                 width: double.infinity,
                 child: const Text(
-                  "hi",
+                  "Expenses Chart",
                   style: TextStyle(
                     fontSize: 23,
                     color: Colors.white,
                   ),
                 )),
           ),
-          Card(
-            elevation: 10,
-            color: Colors.lightBlueAccent,
-            child: Container(
-
-                // width:double.infinity,
-                child: const Text(
-              "hi",
-              style: TextStyle(
-                fontSize: 23,
-                color: Colors.white,
-              ),
-            )),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: (Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: const EdgeInsets.all(15),
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                            fontSize: 20),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title,
+                          style: const TextStyle(
+                              fontSize: 17, fontStyle: FontStyle.italic),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style:
+                              TextStyle(color: Colors.grey[700], fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
+              );
+            }).toList(),
           ),
         ],
       ),
---
+++ app2/lib/transaction.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    @required this.amount,
+    @required this.date,
+    @required this.id,
+    @required this.title,
+  });
+}
